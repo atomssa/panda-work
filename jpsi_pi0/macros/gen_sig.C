d398e0629ee6e05cd7a15dfb57cfb4eb9e57bc12
Ermias Atomssa <atomssa@ipno.in2p3.fr>
2014-08-22 19:31:43 +0200

    close to final signal generation macro

---
+++ jpsi_pi0/macros/gen_sig.C
@@ -0,0 +1,23 @@
+void gen_sig()
+{
+
+  Float_t mom = 6.5; // TODO: will hav to be fixed
+  Int_t nEvents = 10000;
+  
+  gROOT->Macro("$VMCWORKDIR/gconfig/rootlogon.C");
+  gSystem->Load("libEvtGenSA");
+  gSystem->ListLibraries();
+
+  PndEvtGenStandAlone *gen = new PndEvtGenStandAlone("pbarpSystem", "jpsi_pi0.dec", mom);
+  gen->Initialize();
+
+  gen->SetVerbose(0);
+  
+  for (int i=0; i<nEvents; ++i) {
+    gen->Exec();
+  }
+
+  gen->Finalize();
+
+}  
+  
