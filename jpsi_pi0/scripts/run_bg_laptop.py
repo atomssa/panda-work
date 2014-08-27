f7608865d3049f6c64edc6613fbe65bb94ffa111
Ermias Atomssa <etatomssa@gmail.com>
2014-08-27 09:42:51 +0200

    New macro to run simulation on laptop

---
+++ jpsi_pi0/scripts/run_bg_laptop.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+import sys
+import my_utils
+import full_sim
+
+my_utils.sim_type = my_utils.sim_bg
+my_utils.base_dir = "/Users/tujuba/panda/work/jpsi_pi0"
+my_utils.uniq_id = 0
+my_utils.test_run = False
+my_utils.dpm_nevt_per_file=1000
+full_sim.run()
+
+
+#start=int(sys.argv[1])
+#number=int(sys.argv[2])
+#series = range(start, start+number)
+#for i in series:
+#    my_utils.uniq_id = i
+#    my_utils.dbg_msg("run_series: doing uniq id %d" % my_utils.uniq_id)
+#    full_sim.run()
+
+
+
+
+#series = []
+#
+#arg=int(sys.argv[1])
+#
+#if arg == 1:
+#    series.append(0)
+#    series.append(3)
+#    series.append(11)
+#    for i in range(14, 30):
+#        series.append(i)
+#if arg == 2:
+#    series.append(31)
+#    series.append(34)
+#    series.append(35)
+#    series.append(38)
+#    series.append(39)
+#    series.append(41)
+#    series.append(44)
+#    for i in range(45, 61):
+#        series.append(i)
+#
+#print "series: %s" % series
+#
+#my_utils.test_run = False
+#
+#for i in series:
+#    my_utils.uniq_id = i
+#    my_utils.dbg_msg("run_series: doing uniq id %d" % my_utils.uniq_id)
+#    full_sim.run()
