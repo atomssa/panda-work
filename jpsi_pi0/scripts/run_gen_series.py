afb0373e25423c6e3b6daaa0850acff6a490a2e8
Ermias Atomssa <etatomssa@gmail.com>
2014-08-27 10:20:27 +0200

    event generation script updated for background at different energies

--- jpsi_pi0/scripts/run_gen_series.py
+++ jpsi_pi0/scripts/run_gen_series.py
@@ -16,19 +16,30 @@ import evtgen
 #        my_utils.dpm_pbar_lab_mom = 20.0
 #    evtgen.generate()
 
-my_utils.dpm_pbar_lab_mom = 5.513
-my_utils.test_run = False
-s1 = range(1000, 1100, 3)
-s2 = range(1001, 1100, 3)
-s = []
-for idx in range(0,len(s1)-1):
-    s.append(s1[idx])
-    if (idx < len(s2)):
-        s.append(s2[idx])
-print s
-for i in s:
+#my_utils.dpm_pbar_lab_mom = 5.513
+#my_utils.test_run = False
+#s1 = range(1000, 1100, 3)
+#s2 = range(1001, 1100, 3)
+#s = []
+#for idx in range(0,len(s1)-1):
+#    s.append(s1[idx])
+#    if (idx < len(s2)):
+#        s.append(s2[idx])
+#print s
+#for i in s:
+#    my_utils.uniq_id = i
+#    my_utils.dbg_msg( "run_gen_series: doing uniq id %d" % my_utils.uniq_id)
+#    evtgen.generate()
+
+plabs = [8.0, 14.0]
+plab_index = int(sys.argv[1])
+my_utils.sim_type = my_utils.sim_bg
+my_utils.dpm_pbar_lab_mom = plabs[plab_index]
+
+start=int(sys.argv[2])
+number=int(sys.argv[3])
+series = range(start, start+number)
+for i in series:
     my_utils.uniq_id = i
-    my_utils.dbg_msg( "run_gen_series: doing uniq id %d" % my_utils.uniq_id)
+    my_utils.dbg_msg( "run_gen_series: doing uniq id %d at energy %f" % (my_utils.uniq_id,my_utils.dpm_pbar_lab_mom))
     evtgen.generate()
-
-
