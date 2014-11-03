18335a196af7e2f136986d793ba65e52c7cdf40a
Ermias Atomssa <etatomssa@gmail.com>
2014-11-03 02:42:42 +0100

    Fix inv. mass plot according to referee comment for Meson2014 proceeding

--- threepion/plot.C
+++ threepion/plot.C
@@ -1,7 +1,23 @@
+void set_style(TH1* h, int col=0) {
+  h->GetXaxis()->SetTitleSize(0.05);
+  h->GetXaxis()->SetLabelSize(0.05);
+  h->GetYaxis()->SetTitleSize(0.05);
+  h->GetYaxis()->SetLabelSize(0.05);
+  h->GetYaxis()->SetTitleOffset(1.2);
+
+  if (col>0) {
+    h->SetLineWidth(3);
+    h->SetLineColor(col);
+  }
+}
+
 plot() {
 
   gStyle->SetOptStat(0);
-  
+  gStyle->SetPadLeftMargin(0.13);
+  gStyle->SetPadBottomMargin(0.13);
+
+
   TFile *dpm = TFile::Open("output_DPM_PipPimPi0.dat.root");
 
   TFile *ftf = TFile::Open("output_FTF_PipPimPi0.dat.root");
@@ -17,6 +33,9 @@ plot() {
   minv_pippim_dpm->Rebin(iRebin);
   minv_pippim_ftf->Rebin(iRebin);
 
+  set_style(minv_pippim_dpm);
+  set_style(minv_pippim_ftf);
+
   the_pi0_cm_dpm->SetLineColor(4);
   minv_pippim_dpm->SetLineColor(4);
 
@@ -24,15 +43,20 @@ plot() {
   tl_the->AddEntry(the_pi0_cm_dpm,"#theta^{*}_{#pi^{0}} in DPM","l");
   tl_the->AddEntry(the_pi0_cm_ftf,"#theta^{*}_{#pi^{0}} in FTF","l");
   tl_the->SetBorderSize(0);
-  tl_the->SetFillStyle(0);  
-  
+  tl_the->SetFillStyle(0);
+
 
   TCanvas *minv = new TCanvas("minv","minv",700,700);
   minv->cd();
-  minv_pippim_ftf->SetTitle("#bar{p}p#rightarrow#pi^{+}#pi^{-}#pi^{0} @ mom(#bar{p})=5.513 GeV, s=12.25 GeV^{2};M_{inv}^{#pi^{+}#pi^{-}} [GeV/c^2]; dN/dM_{inv}");
-  minv_pippim_ftf->Draw();  
+  minv_pippim_ftf->SetTitle(";M_{inv}^{#pi^{+}#pi^{-}} [GeV/c^{2}]; dN/dM_{inv}");
+  minv_pippim_ftf->Draw();
   minv_pippim_dpm->Draw("same");
   minv_pippim_dpm->SetTitle("");
+
+  TLatex *tt = new TLatex();
+  tt->DrawLatexNDC(0.25,0.3,"#bar{p}p#rightarrow#pi^{+}#pi^{-}#pi^{0}");
+  tt->SetTextSize(0.035);
+  tt->DrawLatexNDC(0.25,0.25,"#sqrt{s} = 3.5 GeV");
   //2.96 < mom_pich.M() && mom_pich.M() < 3.22
   TLine *tlb = new TLine(2.96,0,2.96,0.65*minv_pippim_dpm->GetMaximum());
   tlb->SetLineWidth(2);
@@ -42,13 +66,15 @@ plot() {
   tlu->Draw();
   TLegend *tl_minv = new TLegend(0.5,0.65,0.9,0.9);
   tl_minv->AddEntry(minv_pippim_dpm,"M_{inv}^{#pi^{+}#pi^{-}} in DPM","l");
-  tl_minv->AddEntry(minv_pippim_ftf,"M_{inv}^{#pi^{+}#pi^{-}} in FTF","l");  
-  tl_minv->AddEntry(tlu,"2#sigma M cut (J/#psi)","l");  
+  tl_minv->AddEntry(minv_pippim_ftf,"M_{inv}^{#pi^{+}#pi^{-}} in FTF","l");
+  tl_minv->AddEntry(tlu,"2#sigma M cut (J/#psi)","l");
   tl_minv->SetBorderSize(0);
-  tl_minv->SetFillStyle(0);  
+  tl_minv->SetFillStyle(0);
   tl_minv->Draw();
-  minv->Print("minv.eps");
-  
+  minv->Print("minv.pdf");
+
+  return;
+
   TCanvas *the = new TCanvas("the","the",700,700);
   the->cd();
   the_pi0_cm_ftf->SetTitle("#bar{p}p#rightarrow#pi^{+}#pi^{-}#pi^{0} @ mom(#bar{p})=5.513 GeV, s=12.25 GeV^{2};#theta^{*}_{#pi^{0}};dN/d#theta^{*}_{#pi^{0}}");
@@ -58,6 +84,5 @@ plot() {
   tl_the->Draw();
   minv->Print("the.eps");
 
-  
-}
 
+}
