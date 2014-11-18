39d7931d60038660ced3944a16bd534980c76fde
Ermias Atomssa <etatomssa@gmail.com>
2014-11-18 18:37:40 +0100

    use env. varaible instead of separate macros for work on laptop or fix

---
+++ jpsi_pi0/macros/gen_sig.C
@@ -0,0 +1,38 @@
+void gen_sig()
+{
+
+  Float_t mom = 5.513;
+  // TODO - there are two places where this is filled (here and in decay description file). WHY?
+  Int_t nEvents = 10;
+
+  gROOT->Macro("$VMCWORKDIR/gconfig/rootlogon.C");
+  gSystem->Load("libEvtGenSA");
+  gSystem->ListLibraries();
+
+  string decfile, base = gSystem->Getenv("PNDDIR");
+  cout << "base= \"" << base << "\"" << endl;
+  if (base == "") {
+    cout << "Make sure PNDDIR is set to the root PANDA work directory exiting" << endl;
+    return;
+  } else {
+    decfile = Form("%s/work/jpsi_pi0/macros/jpsi_pi0.dec",base.c_str());
+    std::ifstream inf(decfile.c_str());
+    if (!inf.good()) {
+      cout << "Decay description file " << decfile << " does not exist. exiting" << endl;
+      return;
+    }
+    inf.close();
+  }
+
+  PndEvtGenStandAlone *gen = new PndEvtGenStandAlone("pbarpSystem", decfile, mom);
+  gen->Initialize();
+
+  gen->SetVerbose(0);
+
+  for (int i=0; i<nEvents; ++i) {
+    gen->Exec();
+  }
+
+  gen->Finalize();
+
+}
