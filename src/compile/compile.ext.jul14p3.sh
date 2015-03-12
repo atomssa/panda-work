3adfca90a39640ab53356f3e4363d346521d89e2
Ermias Atomssa <etatomssa@gmail.com>
2015-03-12 18:32:33 +0100

    fix external compilation issues on OSX
    - v5-34-26 of ROOT used to compile on OSX. Version picked by default
      jul14p3 compilation script still had a weird header bug with osx
      clang. Picked the last version of the root 5.X family
    
    - CUDA and Protobuf are non required packages for FairROOT. comment them
      out in CMakeLists.txt file because they cause crash during compilation

--- src/compile/compile.ext.jul14p3.sh
+++ src/compile/compile.ext.jul14p3.sh
@@ -21,6 +21,13 @@ git clone https://github.com/FairRootGroup/FairSoft jul14p3
 cd $SOFT_DIR/jul14p3
 git checkout -b jul14p3 jul14p3
 
+# macbook can't compile early versions of root
+if [[ $HN == "rasalula" ]]; then
+    cd $SOFT_DIR/jul14p3/scripts
+    sed -i.bkp.0 s/'ROOTVERSION=87c1d2a0e691781d2ac93a4f05afee1436feaf4a'/'ROOTVERSION=v5-34-26'/ package_versions.sh
+    cd $SOFT_DIR/jul14p3
+fi
+
 if [[ $HN == "ipnphen01" || $HN == "rasalula" ]]; then
     . ./configure.sh <<EOF
 1
@@ -31,9 +38,9 @@ $SOFT_DIR/jul14p3/install
 2
 EOF
 else
-    sed -i.org.0 s/geant4_download_install_data_automatic=no/geant4_download_install_data_automatic=yes/ configure.sh
-    sed -i.org.1 s/geant4_install_data_from_dir=yes/geant4_install_data_from_dir=no/ configure.sh
-    sed -i.org.2 s/'SIMPATH_INSTALL=$PWD\/installation'/'export SIMPATH_INSTALL=$PWD\/install'/ configure.sh
+    sed -i.bkp.0 s/geant4_download_install_data_automatic=no/geant4_download_install_data_automatic=yes/ configure.sh
+    sed -i.bkp.1 s/geant4_install_data_from_dir=yes/geant4_install_data_from_dir=no/ configure.sh
+    sed -i.bkp.2 s/'SIMPATH_INSTALL=$PWD\/installation'/'export SIMPATH_INSTALL=$PWD\/install'/ configure.sh
     . ./configure.sh grid
 fi
 export SIMPATH=$SOFT_DIR/jul14p3/install
@@ -44,6 +51,13 @@ git clone https://github.com/FairRootGroup/FairRoot.git
 FAIRROOT_DIR=$SOFT_DIR/jul14p3/FairRoot
 cd $FAIRROOT_DIR
 git checkout -b v-14.11 v-14.11
+
+# CUDA and PROTObuf not compileable on OSX, and not required
+if [[ $HN == "rasalula" ]]; then
+    sed -i.bkp.0 s/'find_package(CUDA)'/'#find_package(CUDA)'/ CMakeLists.txt
+    sed -i.bkp.0 s/'find_package(Protobuf)'/'#find_package(Protobuf)'/ CMakeLists.txt
+fi
+
 mkdir $FAIRROOT_DIR/build
 cd $FAIRROOT_DIR/build
 cmake -DCMAKE_INSTALL_PREFIX=$FAIRROOT_DIR/install -DUSE_DIFFERENT_COMPILER=TRUE $FAIRROOT_DIR
