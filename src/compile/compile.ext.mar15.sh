36521a33373c6b0eddc48f9a3ff173dba332d465
Ermias Atomssa <etatomssa@gmail.com>
2015-03-31 19:24:54 +0200

    Pipe answers into confgure.sh script for grid compilation

--- src/compile/compile.ext.mar15.sh
+++ src/compile/compile.ext.mar15.sh
@@ -24,13 +24,6 @@ git clone https://github.com/FairRootGroup/FairSoft ${VFS}
 cd $SOFT_DIR/${VFS}
 git checkout -b ${VFS} ${VFS}
 
-# macbook can't compile early versions of root
-#if [[ $HN == "rasalula" ]]; then
-#    cd $SOFT_DIR/${VFS}/scripts
-#    sed -i.bkp.0 s/'ROOTVERSION=87c1d2a0e691781d2ac93a4f05afee1436feaf4a'/'ROOTVERSION=v5-34-26'/ package_versions.sh
-#    cd $SOFT_DIR/${VFS}
-#fi
-
 if [[ $HN == "rasalula" ]]; then
     ./configure.sh <<EOF
 5
@@ -52,10 +45,15 @@ $SOFT_DIR/${VFS}/install
 2
 EOF
 else
-    sed -i.bkp.0 s/geant4_download_install_data_automatic=no/geant4_download_install_data_automatic=yes/ configure.sh
-    sed -i.bkp.1 s/geant4_install_data_from_dir=yes/geant4_install_data_from_dir=no/ configure.sh
-    sed -i.bkp.2 s/'SIMPATH_INSTALL=$PWD\/installation'/'export SIMPATH_INSTALL=$PWD\/install'/ configure.sh
-    ./configure.sh grid
+    ./configure.sh <<EOF
+1
+1
+1
+2
+1
+$SOFT_DIR/${VFS}/install
+2
+EOF
 fi
 export SIMPATH=$SOFT_DIR/${VFS}/install
 
@@ -65,12 +63,6 @@ git clone https://github.com/FairRootGroup/FairRoot.git
 FAIRROOT_DIR=$SOFT_DIR/${VFS}/FairRoot
 cd $FAIRROOT_DIR
 git checkout -b $VFR $VFR
-
-## CUDA and PROTObuf not compileable on OSX, and not required
-#if [[ $HN == "rasalula" ]]; then
-#    sed -i.bkp.0 s/'find_package(CUDA)'/'#find_package(CUDA)'/ CMakeLists.txt
-#    sed -i.bkp.0 s/'find_package(Protobuf)'/'#find_package(Protobuf)'/ CMakeLists.txt
-#fi
 
 mkdir $FAIRROOT_DIR/build
 cd $FAIRROOT_DIR/build
