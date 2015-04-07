358999cedb7a2e84056ec89db32cfa037d4c42d8
Ermias Atomssa <etatomssa@gmail.com>
2015-04-07 11:10:47 +0200

    combined external+pandaroot compile script for grid machines

---
+++ src/compile/compile.mar15.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "compiling externals..."
+
+chmod +x compile.ext.mar15.sh
+./compile.ext.mar15.sh
+
+echo "compiling pandaroot...."
+
+chmod +x compile.pandaroot.sh mar15 mar15
+./compile.pandaroot.sh mar15 mar15
+
+echo "DONE"
