3eb5d8c4f8ef56c6fa9b980798e5733ab8f887e7
Ermias Atomssa <etatomssa@gmail.com>
2015-04-30 16:35:58 +0200

    Corresponding (to previous commit) changes in picture macro

--- jpsi_pi0/caveats/dphimax.C
+++ jpsi_pi0/caveats/dphimax.C
@@ -1,4 +1,4 @@
-void dphimax() {
+void dphimax(int cfg=-1) {
 
   static const int nconfig = 22;
   Double_t config[nconfig][3] = {
@@ -10,23 +10,56 @@ void dphimax() {
     {2.0, 10.0, 20.0}, {2.5, 10.0, 20.0}};
 
   TCanvas *tc = new TCanvas("tc","tc");
-  tc->Divide(6,4);
+  if (cfg==-1) tc->Divide(6,4);
 
   TLatex *tt = new TLatex();
   tt->SetTextSize(0.07);
   tt->SetNDC(kTRUE);
 
+  bool sep = true;
   for (int iconfig = 0; iconfig < nconfig; ++iconfig) {
+    if (cfg!=-1&&iconfig!=cfg) continue;
     TString fname = Form("../grid.out/psim_oct14_binsong_configs/bremcorr_merged.%d_hists.root",iconfig);
     cout << "Opening file " << fname << endl;
     TFile *f = TFile::Open(fname);
-    TH1F* phi_max_rec = (TH1F*) f->Get("h_dphi_max_rec");
-    TH1F* phi_max_mc = (TH1F*) f->Get("h_dphi_max_mc");
-    tc->cd(1+iconfig);
+
+    TH1F* phi_max_rec = (TH1F*) f->Get("h_dphi_max_rec"); phi_max_rec->SetLineWidth(2);
+    TH1F* phi_max_mc = (TH1F*) f->Get("h_dphi_max_mc"); phi_max_mc->SetLineWidth(2);
+    TH1F* dphi_all_sep = (TH1F*) f->Get("h_dphi_all_sep"); dphi_all_sep->SetLineWidth(2);
+    TH1F* dphi_brem_sep = (TH1F*) f->Get("h_dphi_brem_sep"); dphi_brem_sep->SetLineWidth(2);
+    TH1F* dphi_cut_sep = (TH1F*) f->Get("h_dphi_cut_sep"); dphi_cut_sep->SetLineWidth(2);
+    TH1F* dphi_all_mrg = (TH1F*) f->Get("h_dphi_all_mrg"); dphi_all_mrg->SetLineWidth(2);
+    TH1F* dphi_cut_mrg = (TH1F*) f->Get("h_dphi_cut_mrg"); dphi_cut_mrg->SetLineWidth(2);
+
+    if (cfg==-1)
+      tc->cd(1+iconfig);
+    else
+      tc->cd();
+
     phi_max_mc->Draw();
     phi_max_rec->Draw("same");
     phi_max_rec->SetLineColor(2);
     phi_max_mc->SetMinimum(1);
+
+    if (sep) {
+      dphi_all_sep->Scale(double(phi_max_mc->GetEntries())/double(dphi_all_sep->GetEntries()));
+      dphi_all_sep->SetLineColor(4);
+      dphi_all_sep->Draw("same");
+      dphi_brem_sep->Scale(double(phi_max_mc->GetEntries())/double(dphi_brem_sep->GetEntries()));
+      dphi_brem_sep->SetLineColor(1);
+      dphi_brem_sep->Draw("same");
+      dphi_cut_sep->Scale(double(phi_max_mc->GetEntries())/double(dphi_cut_sep->GetEntries()));
+      dphi_cut_sep->SetLineColor(3);
+      dphi_cut_sep->Draw("same");
+    } else {
+      dphi_all_mrg->Scale(double(phi_max_mc->GetEntries())/double(dphi_all_mrg->GetEntries()));
+      dphi_all_mrg->SetLineColor(4);
+      dphi_all_mrg->Draw("same");
+      dphi_cut_mrg->Scale(double(phi_max_mc->GetEntries())/double(dphi_cut_mrg->GetEntries()));
+      dphi_cut_mrg->SetLineColor(1);
+      dphi_cut_mrg->Draw("same");
+    }
+
     gPad->SetLogy();
     tt->SetTextColor(1);
     tt->SetTextSize(0.07);
