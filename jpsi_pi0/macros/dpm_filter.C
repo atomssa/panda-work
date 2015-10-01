31c4a659296cf488edfdc67d1b9444eb9fda7bc7
Ermias Atomssa <etatomssa@gmail.com>
2015-10-01 16:23:11 +0200

    Enable filtering other types of final state bg reactions
    - introduce structure to carry pdg codes of tracks in event reference
    - save a tree for each event reference with corresponding name and title

--- jpsi_pi0/macros/dpm_filter.C
+++ jpsi_pi0/macros/dpm_filter.C
@@ -7,46 +7,38 @@
 #include <string>
 #include <algorithm>
 
-bool compare_unsorted(const vector<int> &evt, const vector<int> &ref) {
+enum {
+  phot=22,
+  ep=-11,
+  em=11,
+  pi0=111,
+  pip=211,
+  pim=-211,
+  k0s=310,
+  kp=321,
+  km=-321,
+  lambda=3122,
+  alambda=-3122,
+  phi=333,
+  jpsi=443,
+  prot=2212,
+  aprot=-2212,
+  neut=2112,
+  aneut=-2112,
+  wtf=130
+};
 
-  if (evt.size()!=ref.size()) return false;
-  vector<int> visited;
-  for (unsigned int ievt=0; ievt<evt.size(); ++ievt) {
-
-    // search in reference, and mark index visited if foun
-    bool found = false;
-    for (unsigned int iref=0; iref<ref.size(); ++iref) {
-      if ( evt[ievt] == ref[iref]) {
-	found = true;
-
-	// check if this index of ref was already
-	bool was_visited = false;
-	for (unsigned int ivis=0; ivis<visited.size(); ++ivis) {
-	  if (int(iref) == visited[ivis]) {
-	    was_visited = true;
-	    break;
-	  }
-	}
-	if (was_visited) continue;
-
-	visited.push_back(iref);
-	break;
-      }
-    }
-
-    if (!found) return false;
-
-  }
-  return true;
-}
-
-bool compare_sorted(const vector<int> &evt, const vector<int> &ref) {
-  if (evt.size()!=ref.size()) return false;
-  for (unsigned int ievt=0; ievt<evt.size(); ++ievt) {
-    if (evt[ievt]!=ref[ievt]) return false;
-  }
-  return true;
-}
+struct evt_ref {
+  evt_ref(string n, string t, int i1, int i2):count(0), name(n), title(t) { pdg.push_back(i1); pdg.push_back(i2); sort_evt(); }
+  evt_ref(string n, string t, int i1, int i2, int i3): evt_ref(n,t,i1,i2) { pdg.push_back(i3); sort_evt(); }
+  evt_ref(string n, string t, int i1, int i2, int i3, int i4): evt_ref(n,t,i1,i2,i3) { pdg.push_back(i4); sort_evt(); }
+  evt_ref(string n, string t, int i1, int i2, int i3, int i4, int i5): evt_ref(n,t,i1,i2,i3,i4) { pdg.push_back(i5); sort_evt(); }
+  void sort_evt() {std::sort(pdg.begin(),pdg.end());}
+  std::vector<int> pdg;
+  int count;
+  string name;
+  string title;
+};
 
 void print_event(const vector<int> &evt) {
     cout << "---> Event pdg codes : (";
@@ -57,59 +49,129 @@ void print_event(const vector<int> &evt) {
     cout << ")" << endl;
 }
 
-void print_log(int ievt, int npart, vector<int> &pdg_codes){
-  std::cout << "Event(Type3Pi) " << ievt << " Tca.entries= " << npart << std::endl;
+void print_log(int ievt, const vector<int> &pdg_codes){
+  std::cout << "Event(Type3Pi) " << ievt << " Tca.entries= " << pdg_codes.size() << std::endl;
   print_event(pdg_codes);
   cout << endl;
 }
 
+bool compare_unsorted(const vector<int> &evt, const vector<int> &_ref) {
 
-void dpm_filter(string file_name_in) {
+  if (evt.size()!=_ref.size()) return false;
+  vector<int> visited;
+  for (unsigned int ievt=0; ievt<evt.size(); ++ievt) {
+
+    // search in _reference, and mark index visited if foun
+    bool found = false;
+    for (unsigned int i_ref=0; i_ref<_ref.size(); ++i_ref) {
+      if ( evt[ievt] == _ref[i_ref]) {
+	found = true;
+
+	// check if this index of _ref was already
+	bool was_visited = false;
+	for (unsigned int ivis=0; ivis<visited.size(); ++ivis) {
+	  if (int(i_ref) == visited[ivis]) {
+	    was_visited = true;
+	    break;
+	  }
+	}
+	if (was_visited) continue;
+
+	visited.push_back(i_ref);
+	break;
+      }
+    }
+
+    if (!found) return false;
+
+  }
+  return true;
+}
+
+bool compare_sorted(const vector<int> &evt, const vector<int> &_ref) {
+  if (evt.size()!=_ref.size()) return false;
+  for (unsigned int ievt=0; ievt<evt.size(); ++ievt) {
+    if (evt[ievt]!=_ref[ievt]) return false;
+  }
+  return true;
+}
+
+bool compare_sorted(const vector<int> &evt, evt_ref &_ref) {
+  if (compare_sorted(evt, _ref.pdg)) {
+    _ref.count++;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+bool interesting_event(const vector<int> &evt) {
+  for (unsigned int ipdg=0; ipdg<evt.size(); ++ipdg) {
+    if (evt[ipdg]!=phot&&
+	evt[ipdg]!=pi0&&
+	evt[ipdg]!=pip&&
+	evt[ipdg]!=pim&&
+	evt[ipdg]!=prot&&
+	evt[ipdg]!=aprot&&
+	evt[ipdg]!=neut&&
+	evt[ipdg]!=aneut&&
+	evt[ipdg]!=k0s&&
+	evt[ipdg]!=kp&&
+	evt[ipdg]!=km&&
+	evt[ipdg]!=lambda&&
+	evt[ipdg]!=alambda&&
+	evt[ipdg]!=phi&&
+	evt[ipdg]!=wtf
+	) return true;
+  }
+  return false;
+}
+
+void print_interesting_events(int ievt, const vector<int> &evt) {
+  if (interesting_event(evt)) print_log(ievt, evt);
+}
+
+void dpm_filter(string rxn="pi0pippim") {
 
   bool verb = false;
 
+  std::vector<evt_ref> refs;
+  refs.push_back(evt_ref("pi0pippim", "#pi^{0}#pi^{+}#pi^{-}", pi0, pip, pim)); // pi-, pi0, pi+
+  //refs.push_back(evt_ref("pi0epem", "#pi^{0}e^{+}e^{-}", pi0, ep, em)); // e+, e-, pi0: no e+e- events in DPM
+  //refs.push_back(evt_ref("pi0jpsi", "#pi^{0}J/#psi", pi0, jpsi)); // pi0, jpsi: no jpsi events in DPM
+  refs.push_back(evt_ref("pi0pi0pippim", "#pi^{0}#pi^{0}#pi^{+}#pi^{-}", pi0, pi0, pip, pim)); // pi-, pi0, pi0, pi+
+  refs.push_back(evt_ref("pi0pippimpippim", "#pi^{0}#pi^{+}#pi^{-}#pi^{+}#pi^{-}", pi0, pip, pim, pip, pim)); // pi-, pi-, pi0, pi+, pi+
+  //refs.push_back(evt_ref("pi0pi0jpsi", "#pi^{0}#pi^{0}J/#psi", pi0, pi0, jpsi)); // pi0, pi0, jpsi: no jpsi events in DPM
+
   // input
+  string file_name_in = "Background-micro.root";
   TFile *file_in = TFile::Open(file_name_in.c_str());
   TTree *data_in = (TTree*) file_in->Get("data");
   TClonesArray *part_array = new TClonesArray("TParticle");
   data_in->SetBranchAddress("Particles",&part_array);
   int Nevt = data_in->GetEntries();
 
-  string file_name_out = "Background-nano.root";
+  string file_name_out = "filt_complete.root";
   std::cout << "Filtered output file = " << file_name_out << std::endl;
   TFile *file_out = TFile::Open(file_name_out.c_str(),"RECREATE");
-  TTree *data_out_pipmpi0 = new TTree("data","DPM Background");
-  TTree *data_out_jpsipi0 = new TTree("data_jpsi_pi0","data_jpsi_pi0");
-  data_out_jpsipi0->Branch("Particles", &part_array, 32000, 2);
-  data_out_pipmpi0->Branch("Particles", &part_array, 32000, 2);
-
-  std::vector<int> ref1;
-  ref1.push_back(-211);
-  ref1.push_back(111);
-  ref1.push_back(211);
-  int type1_count = 0;
-
-  std::vector<int> ref2;
-  ref2.push_back(-11);
-  ref2.push_back(11);
-  ref2.push_back(111);
-  int type2_count = 0;
-
-  std::vector<int> ref3;
-  ref3.push_back(443);
-  ref3.push_back(111);
-  int type3_count = 0;
+  std::vector<TTree*> data_out;
+  for (unsigned int iref=0; iref < refs.size(); ++iref) {
+    //data_out.push_back(new TTree(Form("data%d",iref),Form("DPM Background %d",iref)));
+    if (rxn==refs[iref].name)
+      data_out.push_back(new TTree("data",refs[iref].title.c_str()));
+    else
+      data_out.push_back(new TTree(refs[iref].name.c_str(),refs[iref].title.c_str()));
+    data_out[iref]->Branch("Particles", &part_array, 32000, 2);
+  }
 
   for (int ievt=0; ievt<Nevt; ievt++) {
 
-    if (ievt%10000==0)
-      std::cout << "event " << ievt << "/" << Nevt << std::endl;
+    if (ievt%10000==0) std::cout << "event " << ievt << "/" << Nevt << std::endl;
 
     data_in->GetEntry(ievt);
 
     // veto events with more than three particles in final state
     int npart = part_array->GetEntriesFast();
-    if (npart>3) continue;
 
     std::vector<int> pdg_codes;
     for (int ipart=0; ipart < npart; ++ipart) {
@@ -118,41 +180,29 @@ void dpm_filter(string file_name_in) {
       pdg_codes.push_back(part->GetPDG()->PdgCode());
     }
 
+    print_interesting_events(ievt, pdg_codes);
+
     std::sort(pdg_codes.begin(),pdg_codes.end());
 
-    if (compare_sorted(pdg_codes,ref1)) {
-      if (verb) print_log(ievt, npart, pdg_codes);
-      data_out_pipmpi0->Fill();
-      type1_count++;
+    for (unsigned int iref=0; iref<refs.size(); ++iref) {
+      if (compare_sorted(pdg_codes, refs[iref])) {
+	if (verb) print_log(ievt, pdg_codes);
+	data_out[iref]->Fill();
+      }
     }
-
-    if (compare_sorted(pdg_codes,ref2)) {
-      if (verb) print_log(ievt, npart, pdg_codes);
-      data_out_jpsipi0->Fill();
-      type2_count++;
-    }
-
-    if (compare_sorted(pdg_codes,ref3)) {
-      if (verb) print_log(ievt, npart, pdg_codes);
-      data_out_jpsipi0->Fill();
-      type3_count++;
-    }
-
-
-
 
   }
 
   file_out->cd();
-  data_out_jpsipi0->Write();
-  data_out_pipmpi0->Write();
+  for (unsigned int iref=0; iref<refs.size(); ++iref) {
+    data_out[iref]->Write();
+  }
   file_out->Write();
-
 
   // summary
   cout << "Total Event Count = " << Nevt << endl;
-  cout << "type1(2Pi) count = " << type1_count << endl;
-  cout << "type2(JpsiPi0) count = " << type2_count << endl;
-  cout << "type2(JpsiPi0Undecayed) count = " << type3_count << endl;
+  for (unsigned int iref=0; iref < refs.size(); ++iref) {
+    cout << refs[iref].title << " event type count= " << refs[iref].count << endl;
+  }
 
 }
